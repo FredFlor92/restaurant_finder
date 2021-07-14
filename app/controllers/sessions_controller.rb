82abd588cdd5d1abdb38b8a0aead55609c411449
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-07-13 23:31:41 -0400

    edited Users and Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,15 @@ class SessionsController < ApplicationController
     end 
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(password: params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Sorry, login info was incorrect. Please try again."
+      redirect_to login_path
+    end
   end 
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  
+  root 'sessions#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+ 
   resources :companies
   resources :reviews
   resources :restaurants
